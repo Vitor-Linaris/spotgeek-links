9a1ca1660a056ff2ce331d93a5c2fff72d8cdd89
Vitor-Linaris <linaris@DESKTOP-D7AD2JN>
2024-05-20 10:50:28 -0300

    Feat: add dynamic update in social-links

--- src/js/main.js
+++ src/js/main.js
@@ -10,9 +10,7 @@ function carregar() {
     .then((buttons) => {
       if (Array.isArray(buttons.links)) {
         buttons.links.forEach((button) => {
-          const container = document.querySelector(".section-links");
-
-          const ul = document.createElement("ul");
+          const container = document.querySelector(".container-links");
 
           const li = document.createElement("li");
           li.classList.add("link");
@@ -21,9 +19,8 @@ function carregar() {
           a.href = button.linkTo;
           a.textContent = button.name;
 
-          ul.appendChild(li);
           li.appendChild(a);
-          container.appendChild(ul);
+          container.appendChild(li);
         });
       } else {
         console.error("Os dados não contêm um array de botões:", buttons);
@@ -34,4 +31,44 @@ function carregar() {
     });
 }
 
+function carregarSocialMidia() {
+  fetch("../../db.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("rro ao buscar o arquivo JSON" + response.statusText);
+      }
+
+      return response.json();
+    })
+    .then((socialLinks) => {
+      if (Array.isArray(socialLinks.socialLinks)) {
+        socialLinks.socialLinks.forEach((socialLink) => {
+          const container = document.querySelector(".social-links");
+
+          const li = document.createElement("li");
+          li.classList.add("social-link");
+
+          const a = document.createElement("a");
+          a.href = socialLink.linkTo;
+
+          const img = document.createElement("img");
+          img.src = socialLink.image;
+          img.alt = socialLink.name_social;
+
+          li.appendChild(a);
+          a.appendChild(img);
+
+          container.appendChild(li);
+        });
+      } else {
+        console.error("Os dados não contêm um array de botões:", socialLinks);
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao processar o JSON:", error);
+    });
+}
+
 carregar();
+
+carregarSocialMidia();
